6df269b63597e0c2dc0b6f4dead8a5217abe37a5
DANNYWW001 <lekandaniel005@gmail.com>
2025-11-23 20:07:29 +0100

    clienturl not frontend url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  process.env.FRONTEND_URL, 
+  process.env.CLIENT_URL, 
 ].filter(Boolean);
 
 app.use(
